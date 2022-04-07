018dad5031f2e32e35986e0e25a9c1ed573eba6e
Sanya <25sshah@athenian.org>
2022-04-07 14:29:47 -0700

    Day 1 python notes

--- src/print_input.py
+++ src/print_input.py
@@ -6,3 +6,19 @@
 #############################
 
 # Write code here:
+print("Oui Oui baguette!!")
+
+name ="Sanya is awesome"
+print(f"Hello {name}!! How are you?")
+print("Im great, thanks!Whats ur name?")
+
+name2 = ("Bob :)")
+print(f"My name is {name2}")
+
+
+name3 = input ("What's your favorite color? ")
+print(f"Cool, I like {name3} too!")
+
+
+
+
